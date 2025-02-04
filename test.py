5cd8f7e6a3a9f68950801fa09b65ab3416e38504
rockleaks <rock94507@gmail.com>
2025-02-05 01:02:05 +0530

    hackthebox theme

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 import os
 
-path = ".\\"
+path = ".\imp\\"
 
 file_list = []
 
 for root, dirs, files in os.walk(path):
     for index, filename in enumerate(files):
         #print(os.path.join(root, filename))
-        if '.pdf' in filename:
+        # if '.pdf' in filename:
             # print("<tr>")
             # print("\t<td></td>")
             # print("\t<td></td>")
-            print("\t<td><a href=\"", os.path.join(root, filename), "\">", f'{filename}', "</a></td>", sep="")
+        print("\t<td><a href=\"", os.path.join(root, filename), "\">", f'{filename}', "</a></td>", sep="")
             # print("</tr>")
-            file_list.append(filename)
+        file_list.append(filename)
 
 #print(file_list)
